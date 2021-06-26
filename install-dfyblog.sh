30b1a5913bd1056de8cd97b1b78fe96a76bec048
Martin Cao <martin.g.cao@gmail.com>
2021-06-26 14:16:05 +1000

    Update install script

--- install-dfyblog.sh
+++ install-dfyblog.sh
@@ -7,10 +7,19 @@ fi
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 DEST_DIR=$(wp eval 'echo ABSPATH;')
+OLD_DOMAIN="listbuildingauthority\.com"
+NEW_DOMAIN="dfyblog\.local"
+OLD_ABSPATH="\/home\/mosahost\/listbuildingauthority"
+NEW_ABSPATH=$(wp eval 'echo str_replace(".", "\.", rtrim(str_replace("/", "\\/", ABSPATH), "\/"));')
 
 # Replace the wp-content folder
 rm -rf "$DEST_DIR/wp-content"
-echo $SRC_DIR
 cp -r "$SRC_DIR/wp-content" "$DEST_DIR/wp-content"
 
-# wp db import "$SRC_DIR/app/dfyblog.sql"
+# Perform search replace on sql file
+cp "$SRC_DIR/dfyblog.sql" "$DEST_DIR/dfyblog-adjusted.sql"
+sed -i "s/\/\/$OLD_DOMAIN/\/\/$NEW_DOMAIN/g" "$DEST_DIR/dfyblog-adjusted.sql"
+sed -i "s/$OLD_ABSPATH/$NEW_ABSPATH/g" "$DEST_DIR/dfyblog-adjusted.sql"
+
+# Import database
+wp db import "$DEST_DIR/dfyblog-adjusted.sql"
