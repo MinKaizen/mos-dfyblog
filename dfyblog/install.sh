bd94f1394db2b6dc26bbef59e3ca85b260333e10
Martin Cao <martin.g.cao@gmail.com>
2021-06-29 10:28:09 +1000

    Update install script

--- dfyblog/install.sh
+++ dfyblog/install.sh
@@ -6,20 +6,33 @@ if ! command -v wp &> /dev/null; then
 fi
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "$SRC_DIR/header.sh"
 DEST_DIR=$(wp eval 'echo ABSPATH;')
-OLD_DOMAIN="listbuildingauthority\.com"
+SRC_WP_CONTENT="$SRC_DIR/../wp-content"
+DEST_WP_CONTENT="$DEST_DIR/wp-content"
+SQL_ADJUSTED="$DEST_DIR/dfyblog-adjusted.sql"
+
+# Search and replace values
+OLD_DOMAIN=$DOMAIN_PLACEHOLDER
 NEW_DOMAIN="dfyblog\.local"
-OLD_ABSPATH="\/home\/mosahost\/listbuildingauthority"
+OLD_ABSPATH=$ABSPATH_PLACEHOLDER
 NEW_ABSPATH=$(wp eval 'echo str_replace(".", "\.", rtrim(str_replace("/", "\\/", ABSPATH), "\/"));')
+OLD_TABLE_PREFIX=$TABLE_PREFIX_PLACEHOLDER
+NEW_TABLE_PREFIX=$(wp db prefix)
 
 # Replace the wp-content folder
-rm -rf "$DEST_DIR/wp-content"
-cp -r "$SRC_DIR/wp-content" "$DEST_DIR/wp-content"
+rm -rf $DEST_WP_CONTENT
+cp -r $SRC_WP_CONTENT $DEST_WP_CONTENT
 
 # Perform search replace on sql file
-cp "$SRC_DIR/dfyblog.sql" "$DEST_DIR/dfyblog-adjusted.sql"
-sed -i "s/\/\/$OLD_DOMAIN/\/\/$NEW_DOMAIN/g" "$DEST_DIR/dfyblog-adjusted.sql"
-sed -i "s/$OLD_ABSPATH/$NEW_ABSPATH/g" "$DEST_DIR/dfyblog-adjusted.sql"
+cp "$SRC_DIR/dfyblog.sql" $SQL_ADJUSTED
+sed -i "s/\/\/$OLD_DOMAIN/\/\/$NEW_DOMAIN/g" $SQL_ADJUSTED
+sed -i "s/$OLD_ABSPATH/$NEW_ABSPATH/g" $SQL_ADJUSTED
+sed -i "s/$OLD_TABLE_PREFIX/$NEW_TABLE_PREFIX/g" $SQL_ADJUSTED
 
 # Import database
-wp db import "$DEST_DIR/dfyblog-adjusted.sql"
+wp db import $SQL_ADJUSTED
+# Make sure to update the admin user
+
+# Clean up
+rm $SQL_ADJUSTED
