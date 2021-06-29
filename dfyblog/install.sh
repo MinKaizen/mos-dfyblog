a3b5c7594505bc830fe22e2e298bf55e17bf8076
Martin Cao <martin.g.cao@gmail.com>
2021-06-29 11:42:22 +1000

    FIX: add missing boilerplate

--- dfyblog/install.sh
+++ dfyblog/install.sh
@@ -1,5 +1,50 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+usage() {
+  cat << EOF # remove the space between << and EOF, this is due to web plugin issue
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+
+Script description here.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-f, --flag      Some flag description
+-p, --param     Some param description
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  rm $SQL_ADJUSTED
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
 parse_params() {
   while :; do
     case "${1-}" in
@@ -33,6 +78,9 @@ parse_params() {
 }
 
 parse_params "$@"
+setup_colors
+
+# START MAIN SCRIPT
 
 if ! command -v wp &> /dev/null; then
   echo "ERROR: wp-cli is not installed"
@@ -70,5 +118,9 @@ wp db import $SQL_ADJUSTED
 # Update admin user
 wp db user update 1 --user_pass=$email --user_nicename=$name --user_email=$email --display_name=$name --nickname=$name --first_name=$name --role=administrator --skip-email
 
-# Clean up
-rm $SQL_ADJUSTED
+# END MAIN SCRIPT
+
+msg "${RED}Read parameters:${NOFORMAT}"
+msg "- flag: ${flag}"
+msg "- param: ${param}"
+msg "- arguments: ${args[*]-}"
